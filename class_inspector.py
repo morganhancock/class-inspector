13863c33857cd9c983972c45f3ea12b1740a5e27
jshuster7484 <jshuster7484@gmail.com>
2016-11-17 21:42:00 -0600

    Adding more tests!

--- class_inspector.py
+++ class_inspector.py
@@ -5,7 +5,18 @@ from bs4 import BeautifulSoup
 proj_path = sys.argv[1]
 
 APPROVED_TAGS = ['Topics', 'Sections', 'HomePage', 'Training', 'Search']
-DEFAULT_TAG = ['Topics']
+DEFAULT_HTML_TAG = ['Topics']
+DEFAULT_IMG_CLASS = ['']
+
+
+class Report:
+    absolute_path = r'C:\...'
+
+
+class TestResult:
+    pass_fail = False
+
+
 
 def warning():
     """Issues a warning and returns True or False based on user input"""
@@ -41,41 +52,73 @@ def make_soup(file):
     return soup
 
 
+def test_images(soup, file, message):
+    if file != "home.htm":
+        for tag in soup.find_all('img'):
+            if tag.parent.name == 'figure':
+                tag.parent.wrap(soup.new_tag("div"))
+                del tag['style']
+                del tag['class']
+                tag['class'] = ['img-responsive center-block']
+                print(soup)
+
+def test_a_element(soup, file, message):
+    for tag in soup.find_all('a'):
+        name = tag.get('name')
+        href = tag.get('href')
+        if name and not href:
+            tag['class'] = ['anchor']
+            print(tag)
+
+
+
+
+def test_class(soup, file, message):
+    for tag in soup.find_all('html'):
+        # REMEMBER: normally you can just define a single word for an
+        # attribute, but class is a special flower that requires a list,
+        # even if that list is only one element long.
+        html_class = tag.get('class')
+        if not html_class:
+            message += '\nNo class tag in html element of file {0}'.format(file)
+            message += '\nDefault class tag "{0}" was added'.format(DEFAULT_HTML_TAG)
+            message += '\n' + '#'*100
+            tag['class'] = DEFAULT_HTML_TAG
+        else:
+            if len(html_class) > 1:
+                new_tag = []
+                for subclass in tag['class']:
+                    if subclass in APPROVED_TAGS:
+                        if len(new_tag) > 0:
+                            message += '\nMultiple approved class tags "{0}" found in html element of file {1}'.format(html_class, file)
+                            message += '\nNO CHANGES WERE MADE'
+                            message += '\n' + '#'*100
+                            break
+                        else:
+                            new_tag.append(subclass)
+                if not new_tag:
+                    message += '\nMultiple non-approved class tags "{0}" found in html element of file {1}'.format(html_class, file)
+                    message += '\n' + '#'*100
+                    tag['class'] = new_tag
+            elif html_class[0] not in APPROVED_TAGS:
+                message += '\nNon-approved class tag "{0}" found in html element of file {1}'.format(html_class[0], file)
+                message += '\n' + '#'*100
+                tag['class'] = DEFAULT_HTML_TAG
+
+    print(message)
+
+    # print(file)
+    # with open(file, 'w', encoding='utf-8-sig') as o:
+    #	o.write(str(soup))
+
 def main():
     if warning():
         message = ''
         for file in get_contents(proj_path):
             soup = make_soup(file)
-            for tag in soup.find_all('html'):
-                # REMEMBER: normally you can just define a single word for an
-                # attribute, but class is a special flower that requires a list,
-                # even if that list is only one element long.
-                html_class = tag.get('class')
-                if not html_class:
-                    message += '\nNo class tag in html element of file {0}'.format(file)
-                    message += '\nDefault class tag "{0}" was added'.format(DEFAULT_TAG)
-                    message += '\n' + '#'*100
-                    tag['class'] = DEFAULT_TAG
-                else:
-                    if len(html_class) > 1:
-                        new_tag = []
-                        for subclass in tag['class']:
-                            if subclass in APPROVED_TAGS:
-                                if len(new_tag) > 0:
-                                    message += '\nMultiple approved class tags "{0}" found in html element of file {1}'.format(html_class, file)
-                                    message += '\nNO CHANGES WERE MADE'
-                                    message += '\n' + '#'*100
-                                    break
-                                else:
-                                    new_tag.append(subclass)
-                        if not new_tag:
-                            message += '\nMultiple non-approved class tags "{0}" found in html element of file {1}'.format(html_class, file)
-                            message += '\n' + '#'*100
-                            tag['class'] = new_tag
-                    elif html_class[0] not in APPROVED_TAGS:
-                        message += '\nNon-approved class tag "{0}" found in html element of file {1}'.format(html_class[0], file)
-                        message += '\n' + '#'*100
-                        tag['class'] = DEFAULT_TAG
+            #test_images(soup, file, message)
+            # test_class(file, message)
+            test_a_element(soup, file, message)
 
         print(message)
 
